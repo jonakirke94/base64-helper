f56c0715bc97198e59a42e6a06fb73957b549599
Jonathan Kirkegaard <jki@commentor.dk>
2021-10-11 15:22:42 +0200

    first commit

---
+++ Base64Helper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Base64Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var base64str = "";
+            byte[] fileArray = Convert.FromBase64String(base64str);
+            string path = Directory.GetCurrentDirectory() + "\\" + Guid.NewGuid() + ".pdf";
+            Console.WriteLine(path);
+            File.WriteAllBytes(path, fileArray);
+            new Process
+            {
+                StartInfo = new ProcessStartInfo(path)
+                {
+                    UseShellExecute = true
+                }
+            }.Start();
+        }
+    }
+}
